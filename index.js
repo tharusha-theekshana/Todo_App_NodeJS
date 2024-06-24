90edf6af23344557a50f8cad6f2af289fc8373fc
TharushaTheekshana <tharushat@fintechasiapacific.com>
2024-06-24 13:00:39 +0530

    Setup Database connection

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+
 const PORT = 8000;
+const connectionUrl = "mongodb://localhost:27017/todo_db";
+
+mongoose.connect(connectionUrl).then(() =>{
+    console.log("Connection success .. !");
+}).catch((err) => {
+    console.log("Failed to connect with database .. !" + err.message);
+})
 
 app.set("view engine","ejs");
 
