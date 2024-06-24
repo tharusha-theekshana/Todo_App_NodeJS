68fd8073f71e4e08264896c0f63ba34bd94df226
Tharusha Theekshana <tharushatheekshana@gmail.com>
2024-06-24 23:22:14 +0530

    Add routers

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
+import path from "path";
+import { fileURLToPath } from 'url';
 
 const app = express();
 
 const PORT = 8000;
 const connectionUrl = "mongodb://localhost:27017/todo_db";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 mongoose.connect(connectionUrl).then(() =>{
     console.log("Connection success .. !");
@@ -12,7 +17,41 @@ mongoose.connect(connectionUrl).then(() =>{
     console.log("Failed to connect with database .. !" + err.message);
 })
 
-app.set("view engine","ejs");
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req, res,next) => {
+    try{
+res.render("index");
+    }catch (e) {
+        res.status(500).json({message : e.message});
+    }
+})
+
+app.get("/addTodo",(req, res,next) => {
+    try{
+        res.render("newTodo");
+    }catch (e) {
+        res.status(500).json({message : e.message});
+    }
+})
+
+app.get("/updateTodo",(req, res,next) => {
+    try{
+        res.render("updateTodo");
+    }catch (e) {
+        res.status(500).json({message : e.message});
+    }
+})
+
+app.get("/deleteTodo",(req, res,next) => {
+    try{
+        res.render("deleteTodo");
+    }catch (e) {
+        res.status(500).json({message : e.message});
+    }
+})
+
 
 app.listen(PORT,() => {
     console.log('Server is running on port ' + PORT);
